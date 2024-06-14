9bfe321bc99758adde8f1871b7bf22b96fe64c8c
KhalidShukri <KHALIDSHUKRIALASOW@GMAIL.COM>
2024-06-14 17:12:33 +0300

    Input from keyboard

--- src/exercise2.java
+++ src/exercise2.java
@@ -6,8 +6,9 @@ public class exercise2 {
         System.out.println("Enter the radius of the circle");
         double radius = scanner.nextDouble();
 //        double area = (22 * radius * radius) / 7;
-        double area = Math.PI * radius * radius;
-        System.out.println("The area of the Circles is: " + area);
+        double area = Math.PI * radius * radius ;
+        System.out.println("The area of the Circles is: " + area+ "14" +
+                "cm\u00b2");
         scanner.close();
 
     }
